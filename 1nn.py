45385328ede704f8d788ecc8db885b4e73f70d70
Marina Alonso Poal <43316350+marinalpo@users.noreply.github.com>
2019-04-26 14:55:59 +0200

    Added multidimensionality

--- 1nn.py
+++ 1nn.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 import math
 
 
-def euclidianDistance(p1, p2):
-    dist = math.pow(p1[0, 0]-p2[0, 0], 2)+math.pow(p1[0, 1]-p2[0, 1], 2)
+def euclidianDistance(p1, p2, Ndim):
+    dist = 0
+    for i_dim in range(Ndim):
+        dist = dist + math.pow(p1[i_dim] - p2[i_dim], 2)
     dist = math.sqrt(dist)
     return dist
 
@@ -13,25 +15,28 @@ def plotScatter(Nclas, train, test):
     for i_class in range(Nclas):
         label = 'Class ' + str(i_class+1)
         plt.scatter(train[:, i_class * 2], train[:, i_class * 2 + 1], label=label)
-    plt.scatter(test[0, 0], test[0, 1], label='Test')
+    plt.scatter(test[0], test[1], label='Test')
     plt.title('1NN Distribution')
     plt.legend()
     plt.show()
 
 
-Nclas = 5  # Number of classes
-N = 10  # Number of samples per class
+Nclas = 4  # Number of classes
+N = 50  # Number of samples per class
+Ndim = 2  # Number of dimensions
 
-train = 10*np.random.rand(N, 2*Nclas)
-test = 10*np.random.rand(1, 2)
+train = 10*np.random.rand(N, Ndim * Nclas)
+test = 10*np.random.rand(Ndim)
 
 distances = np.zeros((N, Nclas))
 
 for i_class in range(Nclas):
     for x in range(N):
-        distances[x, i_class] = euclidianDistance(np.reshape(train[x, (i_class * 2, i_class * 2 + 1)], (1, 2)), test)
+        distances[x, i_class] = euclidianDistance(train[x,i_class*Ndim:(i_class+1)*Ndim], test, Ndim)
 
-class_test=np.where(distances == distances.min())
+class_test = np.where(distances == distances.min())
 
-plotScatter(Nclas, train, test)
+if Ndim == 2:
+    plotScatter(Nclas, train, test)
+
 print('Point ', np.around(test, decimals=2), ' is assigned to class', class_test[1]+1)
